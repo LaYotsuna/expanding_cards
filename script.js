4d536bfef804f8349efea0b901c330e3c5d4d078
Larissa Cerqueira <109742068+LaYotsuna@users.noreply.github.com>
2022-12-05 19:48:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const imgs = document.querySelectorAll(".img");
+const body = document.querySelector(".body");
+
+// Func to remove the active class
+const removeActive = function () {
+  for (let img of imgs) {
+    img.classList.remove("active");
+  }
+};
+
+// Adds active class when you click any img
+const addActive = function () {
+  imgs.forEach((img) =>
+    img.addEventListener("click", () => {
+      console.log("Click");
+      removeActive();
+      img.classList.add("active");
+    })
+  );
+};
+
+// Remove active from img when esc is pressed down
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    removeActive();
+  }
+});
+
+addActive();
